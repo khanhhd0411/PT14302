0ac4438820d839008a22c2191a55ab1efef6018d
khanhhd0411 <kienkakax1@gmail.com>
2020-03-25 21:28:18 +0700

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CategoryComponent } from './category/category.component';
-import { ProductComponent } from './product/product.component';
-import { SlideComponent } from './slide/slide.component';
-
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { SlideComponent } from "./slide/slide.component";
+import { CategoryComponent } from "./category/category.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductComponent } from "./product/product.component";
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent, CategoryComponent, ProductComponent, SlideComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SlideComponent,
+    CategoryComponent,
+    ProductListComponent,
+    ProductComponent,
+
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+imports: [BrowserModule, AppRoutingModule];
